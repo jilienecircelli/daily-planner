07e84a96399701bcc53573206f2daa842a897dc5
Jiliene Circelli <jilienecircelli@gmail.com>
2019-10-19 19:51:47 -0700

    updated files

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-var containerDiv9 = $("#9");
-var containerDiv10 = $("#10");
-var containerDiv11 = $("#11");
-
-
-var textareaNine = $("<textarea id = nine></textarea>")
-var textareaTen = $("<textarea id = ten></textarea>")
-var saveBtn9 = $("<button class = saveBtn>Save</button>")
-var saveBtn10 = $("<button class = saveBtn>Save</button>")
-
-
-
-$(containerDiv9).append(textareaNine);
-// .append(saveBtn9)
-
-$(containerDiv10).append(textareaTen);
-// .append(saveBtn10)
-
-
-
 $(document).ready(function() {
     var currentDay = $("#currentDay");
     var date = $("<div>");
     date.text(moment().format('dddd, MMMM Do YYYY'));
     currentDay.append(date);
-    // var apptText = $("<textarea>");
-    // // var apptTextContent = apptText.val();
-    // $(".col-md-10").append(apptText);
-    var clickToSave = $(".saveBtn").on("click", function() {
-        alert("You saved this appointment");
-        var apptTextContent = $(this).siblings(".col-md-10").val();
-        console.log(apptTextContent);
-        localStorage.setItem("apptTextContent", apptTextContent);
-    });
-    clickToSave()
 });
+
+var apptText = $("<textarea>");
+var apptTextContent = apptText.val();
+$(".col-md-10").append(apptText);
+
+
+// var saveFunction = $(".saveBtn").on("click", function clickToSave() {
+//     alert("You saved this appointment!");
+//     var apptTextContent = $(this).siblings(".col-md-10").val();
+//     console.log(apptTextContent);
+
+
+//     var timeSlot = localStorage.getItem("col-md-1");
+//     var appointmentText = localStorage.getItem("col-md-10");
+//     localStorage.setItem(timeSlot, appointmentText);
+//     console.log(timeSlot)
+
+// });
+// clickToSave()
+
+// var pastTime = moment().diff;
+// var presentTime = moment().diff;
+// var futureTime = moment().diff;
+
+
+// moment().isBefore(current.time);
+
+var currentTime = moment()
+console.log(currentTime)
+
+
+var startDay = "09:00 AM"
+var startTime = moment(startDay)
+var endDay = "05:00 PM"
+var endTime = moment(endDay)
+console.log(startTime, endDay)
+var r9 = $("#r9");
+r9.append(startTime)
