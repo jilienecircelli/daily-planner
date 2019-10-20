e5c640d24306b29efd41330f4d8db810c7c2d331
Jiliene Circelli <jilienecircelli@gmail.com>
2019-10-19 20:03:14 -0700

    updated script

--- script.js
+++ script.js
@@ -10,29 +10,20 @@ var apptTextContent = apptText.val();
 $(".col-md-10").append(apptText);
 
 
-// var saveFunction = $(".saveBtn").on("click", function clickToSave() {
-//     alert("You saved this appointment!");
-//     var apptTextContent = $(this).siblings(".col-md-10").val();
-//     console.log(apptTextContent);
+var saveFunction = $(".saveBtn").on("click", function clickToSave() {
+    alert("You saved this appointment!");
+    var apptTextContent = $(this).siblings(".col-md-10").val();
+    console.log(apptTextContent);
 
 
-//     var timeSlot = localStorage.getItem("col-md-1");
-//     var appointmentText = localStorage.getItem("col-md-10");
-//     localStorage.setItem(timeSlot, appointmentText);
-//     console.log(timeSlot)
+    var timeSlot = localStorage.getItem("col-md-1");
+    var appointmentText = localStorage.getItem("col-md-10");
+    localStorage.setItem(timeSlot, appointmentText);
+    console.log(timeSlot)
 
-// });
-// clickToSave()
+});
+clickToSave()
 
-// var pastTime = moment().diff;
-// var presentTime = moment().diff;
-// var futureTime = moment().diff;
-
-
-// moment().isBefore(current.time);
-
-var currentTime = moment()
-console.log(currentTime)
 
 
 var startDay = "09:00 AM"
@@ -41,4 +32,4 @@ var endDay = "05:00 PM"
 var endTime = moment(endDay)
 console.log(startTime, endDay)
 var r9 = $("#r9");
-r9.append(startTime)
+r9.append.moment().hour(9);
